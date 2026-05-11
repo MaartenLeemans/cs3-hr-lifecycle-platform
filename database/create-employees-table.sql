7df14bc264cdd9825aed6e06a4500605235f3492
MaartenLeemans <maartenleemans2004@gmail.com>
2026-05-11 16:18:53 +0200

    Update create-employees-table.sql

--- database/create-employees-table.sql
+++ database/create-employees-table.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    department VARCHAR(50) NOT NULL,
+    status VARCHAR(30) NOT NULL,
+    role VARCHAR(50) NOT NULL
+);
